39829abe6b3335d9221aacbffe7d75cd7c2d0b96
Mike Cohen <mike3k@gmail.com>
2015-02-20 14:56:13 -0800

    Initial Commit

---
+++ pushtest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pushtest
+//
+//  Created by Mike Cohen on 2/20/15.
+//  Copyright (c) 2015 Mike Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
